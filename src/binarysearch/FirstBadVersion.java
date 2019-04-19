b802b012b3f38a25c70e04b7c8c6b352b039b211
kotol@icloud.com <kotol@icloud.com>
2019-04-19 08:55:55 +0300

    new lessons

--- src/binarysearch/FirstBadVersion.java
+++ src/binarysearch/FirstBadVersion.java
@@ -8,20 +8,27 @@ public class FirstBadVersion extends VersionControl{
             while(left < right) {
                 // Prevent (left + right) overflow
                 int mid = left + (right - left) / 2;
-                System.out.println(mid);
-                if(isBadVersion(mid)){ return mid; }
-                else if(true) { left = mid + 1; }
-                else { right = mid; }
+                //System.out.println(mid);
+                if(isBadVersion(mid)){
+                    System.out.println(mid);
+                    right=mid;
+                }
+                else if (!isBadVersion(mid)) {
+                    System.out.println(mid);
+                    left = mid + 1;
+                    right = n;
+                }
             }
 
             // Post-processing:
             // End Condition: left == right
-            if(left != n && isBadVersion(left)) return left;
+            if (left != n && isBadVersion(left)) return left;
+            if (left ==1 && right==1) return 1;
             return -1;
     }
 
     public static void main(String[] args) {
         FirstBadVersion firstBadVersion = new FirstBadVersion();
-        System.out.println(firstBadVersion.firstBadVersion(5));
+        System.out.println(firstBadVersion.firstBadVersion(6));
     }
 }
