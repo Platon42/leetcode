89afcbbe9e5e8181f824c9277c22b0ea9638ac7c
kotol@icloud.com <kotol@icloud.com>
2019-05-13 12:10:05 +0300

    new lessons

---
+++ src/main/java/dynamic/NumDecodings.java
@@ -0,0 +1,15 @@
+package dynamic;
+
+public class NumDecodings {
+
+    public int numDecodings(String s) {
+
+        char[] chars = s.toCharArray();
+
+        for (int i = 0; i < chars.length; i++) {
+
+        }
+
+        return 1;
+    }
+}
